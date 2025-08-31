ebf1233b1f28d800e8459e2671dddb4f32fbc13c
Rahul Srivastava <117383410+rahul-srivastava-codes@users.noreply.github.com>
2025-08-31 21:45:52 +0530

    Update db.js
    If url is not present throw error "MONGO_URI is not defined in environment variables"

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in environment variables");
+}
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log('mongodb connected successfully');
@@ -8,4 +11,5 @@ const connectDB = async () => {
         console.log(error);
     }
 }
+
 export default connectDB;
